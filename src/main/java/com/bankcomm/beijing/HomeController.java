8ef2fe4a1bac59b4303127cb4bd4d3a59af43477
leeewei <leeewei@gmail.com>
2015-12-04 17:55:48 +0800

    Controller增加单文件上传和多文件上传功能

--- src/main/java/com/bankcomm/beijing/HomeController.java
+++ src/main/java/com/bankcomm/beijing/HomeController.java
@@ -4,27 +4,33 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.inject.Inject;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.bankcomm.beijing.hr.service.UserService;
 
 /**
  * Handles requests for the application home page.
  */
-//@Controller
-@RestController
+@Controller
+//@RestController
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	@Inject
+	private UserService userService;
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	
+	@RequestMapping(value = "/users", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
@@ -37,5 +43,24 @@ public class HomeController {
 		
 		return "home";
 	}
-	
+	@RequestMapping(value="/users/import",method=RequestMethod.POST)
+	public void importFromFile(@RequestParam(value="file",required=false) MultipartFile file,Model model){
+		logger.debug("file name:"+file.getOriginalFilename());
+		logger.debug("file size:"+file.getSize());
+	}
+	/**
+	 * 
+	 * @param files 上传的多个文件，html的input元素里name必须跟参数名相同
+	 * @param model
+	 */
+	@RequestMapping(value="/users/imports",method=RequestMethod.POST)
+	public void importFromFile(@RequestParam(value="file1") MultipartFile[] files,Model model){
+		logger.debug("files count:"+files.length);
+		for(MultipartFile file:files){
+			logger.debug("file name:"+file.getOriginalFilename());
+			logger.debug("file size:"+file.getSize());
+			logger.debug("file type:"+file.getContentType());
+			logger.debug("\n");
+		}
+	}
 }
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -1,14 +1,21 @@
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <%@ page session="false" contentType="text/html; charset=UTF-8"%>
 <html>
 <head>
-	<title>Home</title>
+<title>Home</title>
 </head>
 <body>
-<h1>
-	Hello world!  
-</h1>
+	<h1>Hello world!</h1>
 
-<P>  The time on the server is ${serverTime}. </P>
+	<P>The time on the server is ${serverTime}.</P>
+	<form action="users/import" method="post" enctype="multipart/form-data">
+		<input type="file" name="file" />
+		<input type="submit" value="import one file" />
+	</form>
+	<form action="users/imports" method="post"	enctype="multipart/form-data">
+		<input type="file" name="file1" /> 
+		<input type="file" name="file1" />
+		<input type="submit" value="import more file" />
+	</form>
 </body>
 </html>

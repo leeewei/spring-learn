b39e1a181cfe7cedcd8509b2ad312bd9939e09c8
leeewei <leeewei@gmail.com>
2015-12-23 17:43:05 +0800

    单点登出调试ok

--- src/main/java/com/bankcomm/beijing/hr/controller/HomeController.java
+++ src/main/java/com/bankcomm/beijing/hr/controller/HomeController.java
@@ -45,7 +45,7 @@ public class HomeController {
 	private UserService userService;
 	
 	
-	@RequestMapping(value = "/users/home", method = RequestMethod.GET)
+	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
